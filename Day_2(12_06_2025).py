c8ac9508f5211437b1fbdad8422856b4711b156d
Anandraj D S <anandrajds@gmail.com>
2026-06-14 10:27:31 +0530

    Extract Ip address from the string

--- Day_2(12_06_2025).py
+++ Day_2(12_06_2025).py
@@ -23,3 +23,16 @@ if word != word[::-1]:
     print(f"It is not a palindrome '{word}'")
 else:
     print(f"It is a palindrome '{word}'")
+
+print("**************************************************************");
+
+str = "My ip address is 192.168.1.1 and my subnet mask is 255.255.255.0"
+
+def extract_ip_addresses(input_string):
+    import re
+    ip_pattern = r'\b(?:\d{1,3}\.){3}\d{1,3}\b'
+    ip_addresses = re.findall(ip_pattern, input_string)
+    return ip_addresses
+
+ip_addresses = extract_ip_addresses(str)
+print(f"Extracted IP addresses: {ip_addresses}")

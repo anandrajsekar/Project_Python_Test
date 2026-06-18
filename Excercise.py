50e5f4d472f6d32ad58c6c5676122807751f543c
Anandraj D S <anandrajds@gmail.com>
2026-06-18 14:57:57 +0530

    Updated With Output

--- Excercise.py
+++ Excercise.py
@@ -2,10 +2,22 @@
 # BMI = weight/(height^2)
 
 # The BMI Should be classified as follows:
+
 # Underweight - BMI under or equal to 18.5
+
+# Output : Your Weight (Kg) is : '72', Your Height (m) is : '2.0' and Your BMI is : '18.0'. You are in Underweight
+
 # Normal - BMI Between 18.5 and 25 (not included 18.5 but included 25)
+
+# Output : Your Weight (Kg) is : '72', Your Height (m) is : '1.9' and Your BMI is : '19.94459833795014'. You are in Normal
+
 # Overweight - BMI between 25 and 30 ( not included 25 but included 30)
+
+# Output : Your Weight (Kg) is : '72', Your Height (m) is : '1.6' and Your BMI is : '28.124999999999993'. You are in Overweight
+
 # Obese - BMI greater than 30
+
+# Output : Your Weight (Kg) is : '72', Your Height (m) is : '1.5' and Your BMI is : '32.0'. You are in Obese
 
 # Output: Your BMI is '{BMI Value}'. You are '{classified}'
 

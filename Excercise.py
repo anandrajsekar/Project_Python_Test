f4792e6863c976851250f41c96a64159b93a0973
anandrajsekar <anandrajds@gmail.com>
2026-06-18 17:06:26 +0530

    Uploaded With Proper Calc

--- Excercise.py
+++ Excercise.py
@@ -5,25 +5,25 @@
 
 # Underweight - BMI under or equal to 18.5
 
-# Output : Your Weight (Kg) is : '72', Your Height (m) is : '2.0' and Your BMI is : '18.0'. You are in Underweight
+# Output : Your Weight (Kg) is : '72', Your Height (m) is : '1.945' and Your BMI is : '18.5'. You are in Underweight
 
 # Normal - BMI Between 18.5 and 25 (not included 18.5 but included 25)
 
-# Output : Your Weight (Kg) is : '72', Your Height (m) is : '1.9' and Your BMI is : '19.94459833795014'. You are in Normal
+# Output : Your Weight (Kg) is : '72', Your Height (m) is : '1.698' and Your BMI is : '25.0'. You are in Normal
 
 # Overweight - BMI between 25 and 30 ( not included 25 but included 30)
 
-# Output : Your Weight (Kg) is : '72', Your Height (m) is : '1.6' and Your BMI is : '28.124999999999993'. You are in Overweight
+# Output : Your Weight (Kg) is : '72', Your Height (m) is : '1.55' and Your BMI is : '30.0'. You are in Overweight
 
 # Obese - BMI greater than 30
 
-# Output : Your Weight (Kg) is : '72', Your Height (m) is : '1.5' and Your BMI is : '32.0'. You are in Obese
+# Output : Your Weight (Kg) is : '72', Your Height (m) is : '1.546' and Your BMI is : '30.1'. You are in Obese
 
 # Output: Your BMI is '{BMI Value}'. You are '{classified}'
 
 Weight = int(input("Emter Your Weight in Kg : "))
-Height = float(input("Enter Your Height in Cm : "))/100
-BMI = float(Weight/(Height**2))
+Height = round(float(input("Enter Your Height in Cm : "))/100, 3)
+BMI = round(float(Weight/(Height**2)), 1)
 
 if BMI <= 18.5:
 

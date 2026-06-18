86dfce511651d73db525239406a1efc583dcc85c
anandrajsekar <anandrajds@gmail.com>
2026-06-18 20:10:43 +0530

    Adding The Finding Even Number Or Not

---
+++ Even_Number_Or_Not.py
@@ -0,0 +1,9 @@
+Input_Number = input("Enter Any  Number : ")
+
+if float(Input_Number) % 2 == 0:
+
+    print("Enter Number '{}' Is An Even Number".format(Input_Number))
+
+else: 
+
+    print("Enter Number '" + Input_Number + "' Is Not Even Number")

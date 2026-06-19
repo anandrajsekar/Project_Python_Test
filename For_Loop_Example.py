6661e001cd32f1350806cc98d72e70a75e8bb24d
Anandraj D S <anandrajds@gmail.com>
2026-06-19 18:16:21 +0530

    Update The For Loop Test Case

--- For_Loop_Example.py
+++ For_Loop_Example.py
@@ -1,8 +1,16 @@
 string_of_words = "Learning a little each day adds up. Research shows that students who make learning a habit are more likely to reach their goals. Set time aside to learn and get reminders using your learning scheduler."
 
+Collection_Of_Words = []
+
 for word in string_of_words.split():
+    
     if len(word) <=3:
+    
+        Collection_Of_Words.append(word)
+    
         print(word)
+
+print(Collection_Of_Words)    
 
 # Example: One
 
